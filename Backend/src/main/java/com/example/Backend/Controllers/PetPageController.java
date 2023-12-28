90751ea3b0e4f0e578e85050aaf9735da1ca5cd9
Adel Mahmoud <113463584+Adel-Mahmoud-Mohamed@users.noreply.github.com>
2023-12-29 00:22:13 +0200

    PASMS-17-Setting up

---
+++ Backend/src/main/java/com/example/Backend/Controllers/PetPageController.java
@@ -0,0 +1,46 @@
+package com.example.Backend.Controllers;
+
+import com.example.Backend.DTO.CreateShelterRequest;
+import com.example.Backend.DTO.EmployeeDetails;
+import com.example.Backend.DTO.UpdateShelterRequest;
+import com.example.Backend.DTO.UserInfo;
+import com.example.Backend.Services.ManagerService;
+import com.example.Backend.Services.PetPageService;
+import com.example.Backend.Services.Utils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/petPage")
+@CrossOrigin(origins = "http://localhost:3000/")
+@RequiredArgsConstructor
+public class PetPageController {
+
+    private final Utils utils;
+
+    private final PetPageService petPageService;
+    public String extractToken(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7); // Skip "Bearer " prefix
+        } else {
+            throw new IllegalArgumentException("Authorization header doesn't exist or is in the wrong format");
+        }
+    }
+
+    @GetMapping("/getUserInfo")
+    public ResponseEntity<UserInfo> getUserInfo (@RequestHeader("Authorization") String authorizationHeader,
+                                                 @RequestParam("role") String role,
+                                                 @RequestParam("shelterName") String shelterName) {
+        try {
+            String token = extractToken(authorizationHeader);
+            return ResponseEntity.status(HttpStatus.OK).body(utils.getUserInfo(token, role, shelterName));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+    }
+}
---
+++ Backend/src/main/java/com/example/Backend/Services/PetPageService.java
@@ -0,0 +1,15 @@
+package com.example.Backend.Services;
+
+import com.example.Backend.DTO.UserInfo;
+import com.example.Backend.Model.Adopter;
+import com.example.Backend.Model.Employee;
+import com.example.Backend.Model.Manager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PetPageService {
+
+
+}
