29bbbb3ae4fdee107659789ca63cc5c1d3b29bb1
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-12-29 16:47:10 +0200

    PASMS-11-update

---
+++ Frontend/src/Pages/StaffSignUP.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Form from "../Components/Form";
+
+const StaffSignUP = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [responseStatus, setResponseStatus] = useState("");
+  const [staffEmail, setStaffEmail] = useState("adelmahmoud199540@gmail.com");
+  // const []
+  //function to direct the flow to sign up using credentials
+  const getStaffSignUpCredentials = (customer: RegisterRequest) => {
+    handleStaffSignupRequest(customer);
+  };
+
+  const handleStaffSignupRequest = async (customer: RegisterRequest) => {
+    // TODO
+  };
+
+  const handleStaffSignupResponse = () => {
+    //TODO
+  };
+
+  return (
+    <Form
+      isLogin={false}
+      isStaff={true}
+      staffEmail={staffEmail}
+      getStaffSignUpCredentials={getStaffSignUpCredentials}
+    />
+  );
+};
+
+export default StaffSignUP;
