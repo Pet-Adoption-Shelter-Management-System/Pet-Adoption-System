636519e81611350ccd987c77c35b5dae6bc242bb
Adel Mahmoud <113463584+Adel-Mahmoud-Mohamed@users.noreply.github.com>
2023-12-29 03:21:00 +0200

    PASMS-17-staging 2

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,21 +1,10 @@
-import { Button } from "react-bootstrap";
 import "./App.css";
 import SignUp from "./Pages/SignUp";
-import CreateShelter from "./Pages/CreateShelter";
-import PetsPage from "./Pages/PetsPage";
-import PetComponent from "./Components/PetComponent";
 
 function App() {
-
-
-
-
-
-
   return (
     <>
-      {/* <SignUp /> */}
-      <PetComponent/>
+      <SignUp />
     </>
   );
 }
