7501e08f3876a2c68f5bb058ace035a20c4af30f
Mohamed <mo.hassan2792@gmail.com>
2023-12-29 23:28:18 +0200

    PASMS-12-initial implementation of the notification

--- Backend/src/main/java/com/example/Backend/Services/ApplicationService.java
+++ Backend/src/main/java/com/example/Backend/Services/ApplicationService.java
@@ -1,5 +1,6 @@
 package com.example.Backend.Services;
 
+import com.example.Backend.Middleware.Utils;
 import com.example.Backend.Model.Application;
 import com.example.Backend.Model.Pet;
 import com.example.Backend.Model.Shelter;
@@ -9,6 +10,7 @@ import com.example.Backend.Repositories.PetRepository;
 import com.example.Backend.Repositories.ShelterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,14 +19,16 @@ import java.util.Optional;
 
 
 @Service
+@EnableAsync
 @RequiredArgsConstructor
 @Transactional
 public class ApplicationService {
 
-    private final AdopterRepository adopterRepo;
+    private final Utils utils;
     private final PetRepository petRepo;
     private final ShelterRepository shelterRepo;
     private final ApplicationRepository applicationRepo;
+    private final NotificationService notificationService;
 
     public void submitApp(String token, long petId, long shelterId) {
         Optional<Pet> optionalPet = petRepo.findById(petId);
@@ -36,13 +40,14 @@ public class ApplicationService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Shelter not found");
         }
         Application application = Application.builder()
-                .adopter(adopterRepo.findByEmail(token).get())
+                .adopter(utils.getAdopter(token))
                 .shelter(optionalShelter.get())
                 .pet(optionalPet.get())
                 .status("Pending")
                 .date(java.time.LocalDateTime.now())
                 .build();
         applicationRepo.save(application);
+        notificationService.notifyAdopter(application);
     }
 
     public void manageApp(long appId, String status) {
